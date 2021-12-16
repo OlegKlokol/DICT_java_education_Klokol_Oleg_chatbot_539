9dc035cd726b8a3c1019ca5ae3fe92f43cf49456
OlegKlokol <o.v.klokol@student.khai.edu>
2021-12-16 12:15:57 +0200

    ChatBot 1-st stage

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run(args);
+    }
+
+    public static int remainder3;
+    public static int remainder5;
+    public static int remainder7;
+    public static int Age;
+    public static int userNumber;
+    public static int Test;
+    public static int answer;
+
+    public static void run(String[] args) {
+        System.out.println("Hello man! My name is SaintOG\n" +
+                "I was created in 2021.");
+    }
+}
