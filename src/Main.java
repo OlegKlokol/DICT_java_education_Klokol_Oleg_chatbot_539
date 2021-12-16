ef58030ce53b234087fa52e40f2b11982d26e283
OlegKlokol <o.v.klokol@student.khai.edu>
2021-12-16 12:19:47 +0200

    ChatBot 2-nd stage

--- src/Main.java
+++ src/Main.java
@@ -17,30 +17,9 @@ public class Main {
     public static void run(String[] args) {
         System.out.println("Hello man! My name is SaintOG\n" +
                 "I was created in 2021.");
-       import java.util.Scanner;
-
-        public class Main {
-            public static void main(String[] args) {
-                Main main = new Main();
-                main.run(args);
-            }
-
-            public static int remainder3;
-            public static int remainder5;
-            public static int remainder7;
-            public static int Age;
-            public static int userNumber;
-            public static int Test;
-            public static int answer;
-
-            public static void run(String[] args) {
-                System.out.println("Hello man! My name is SaintOG\n" +
-                        "I was created in 2021.");
-                System.out.println("Please, remind me your name\n");
-                Scanner scanner = new Scanner(System.in);
-                String name = scanner.nextLine();
-                System.out.println("What a great name you have, " + name + " !");
-            }
-        }
+        System.out.println("Please, remind me your name\n");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + " !");
     }
 }
